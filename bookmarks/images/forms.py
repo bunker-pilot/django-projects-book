037e747299f0b3ced05ec67f58a12c762dbe274d
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-02-10 09:57:12 +0330

    tseted and fixed some small annoyances

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -9,7 +9,7 @@ class ImageForm(forms.ModelForm):
         model = Image
         fields = ["title" , "url" , "description"]
         widgets = {
-            "url" : forms.HiddenInput,
+            "url" : forms.HiddenInput(),
         }
     def clean_url(self):
         url = self.cleaned_data["url"]
@@ -18,13 +18,14 @@ class ImageForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError("This image format is not supported!")
         return url 
-    def save(self, force_insert=False, force_update=False,commit =True):
+    def save(self,commit =True):
         image = super().save(commit = False)
         image_url = self.cleaned_data["url"]
         name = slugify(image.title)
         extension = image_url.rsplit("." , 1)[1].lower()
         image_name = f"{name}.{extension}"
         response = requests.get(image_url)
+        response.raise_for_status()
         image.image.save(
             image_name, ContentFile(response.content), save = False
         )
